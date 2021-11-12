0d8f34fc4e0f587983b4ef743395493561af14ea
musab <mas.gh123@hotmail.com>
2021-11-12 20:17:02 +0300

    ....

---
+++ app/src/main/java/com/sumaya/hw_week06_day05/PhotosViewModel.kt
@@ -0,0 +1,29 @@
+package com.sumaya.hw_week06_day05
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class PhotosViewModel: BaseObservable()  {
+    var vmPhoto: Results? = null
+        set(vmPhoto){
+            field = vmPhoto
+            notifyChange()
+        }
+
+    @get:Bindable
+    val id : Int?
+        get() = vmPhoto?.id
+
+    @get:Bindable
+    val poster : String?
+        get() = vmPhoto?.poster_path
+    @get:Bindable
+    val date : String?
+        get() = vmPhoto?.release_date
+    @get:Bindable
+    val title : String?
+        get() = vmPhoto?.original_title
+    @get:Bindable
+    val vote : Double?
+        get() = vmPhoto?.vote_average
+}
